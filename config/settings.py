12004f12256f437fb4579ebea81cfcf1337326e3
Ali Farzalisoy <alifarzalisoy099@gmail.com>
2026-02-08 10:30:02 +0400

    cloudinary deploy2

--- config/settings.py
+++ config/settings.py
@@ -118,15 +118,29 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 import cloudinary
+import cloudinary.uploader
+from cloudinary.utils import cloudinary_url
 
-cloudinary.config(
-  cloud_name = "CLOUD_NAME",
-  api_key = "API_KEY",
-  api_secret = "API_SECRET"
+# Configuration       
+cloudinary.config( 
+    cloud_name = "dzshutbh7", 
+    api_key = "429125927774837", 
+    api_secret = "<your_api_secret>", # Click 'View API Keys' above to copy your API secret
+    secure=True
 )
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+# Upload an image
+upload_result = cloudinary.uploader.upload("https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg",
+                                           public_id="shoes")
+print(upload_result["secure_url"])
 
+# Optimize delivery by resizing and applying auto-format and auto-quality
+optimize_url, _ = cloudinary_url("shoes", fetch_format="auto", quality="auto")
+print(optimize_url)
+
+# Transform the image: auto-crop to square aspect_ratio
+auto_crop_url, _ = cloudinary_url("shoes", width=500, height=500, crop="auto", gravity="auto")
+print(auto_crop_url)
 
 
 
